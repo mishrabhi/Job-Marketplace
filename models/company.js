fd29319192a5ab14e7fded8b92dcb1f5932cbfae
Abhishek Mishra <abhi@Abhisheks-MacBook-Air.local>
2026-05-28 00:09:04 +0530

    first commit

---
+++ models/company.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const newCompanySchema = new.mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,"Company name is required"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    description: {
+        type: String,
+        default: ""
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["company"],
+        default: "company"
+    }
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model("Company", newCompanySchema)
+
+
+
